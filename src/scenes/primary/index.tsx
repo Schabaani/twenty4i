2c0e013ea9970e90d682040739b6f912c1da3bb4
Amish <a.shabani@megagroup.biz>
2022-03-30 13:50:13 +0430

    remove old screens

--- src/scenes/primary/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {TPrimaryScreen} from '../../interfaces';
-import {Style} from './styles';
-
-export default function PrimaryScreen({navigation}: TPrimaryScreen) {
-  return <SafeAreaView style={Style.container}></SafeAreaView>;
-}
