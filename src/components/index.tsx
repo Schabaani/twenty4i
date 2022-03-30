2d83c39e08e180ba4b921dbafeda6916a2dbeeff
Amish <a.shabani@megagroup.biz>
2022-03-30 13:48:24 +0430

    Carousel cmp

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 import MySvgImage from './MySvgImage';
 import {Spacer} from './Spacer';
+import {Carousel} from './Carousel';
 
-export {MySvgImage, Spacer};
+export {MySvgImage, Spacer, Carousel};
